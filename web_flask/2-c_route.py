7c9e67afce7fb7a89090d22d9e4acc5210b264af
Aminekham <lkham0508@gmail.com>
2023-04-13 01:24:34 +0200

    adding a variable routing

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""""""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+strict_slashes = False
+
+@app.route('/')
+def hello_HBNB():
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb():
+    return("HBNB")
+
+@app.route('/c/<text>')
+def text(text):
+    t = text.replace("_", " ")
+    return("C {}".format(t))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
