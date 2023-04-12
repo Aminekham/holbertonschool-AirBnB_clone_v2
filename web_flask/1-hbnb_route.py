cd221fed127174b7e1c512f0066adfe558c1202a
Aminekham <lkham0508@gmail.com>
2023-04-13 01:13:04 +0200

    fixing documentation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+"""
+Write a script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
 Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
+
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+
 """
 from flask import Flask
 
