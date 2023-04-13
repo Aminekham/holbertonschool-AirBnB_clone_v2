717cc2633421e7f8dc50733d56b9d3e4b663c0ac
Aminekham <lkham0508@gmail.com>
2023-04-14 00:42:01 +0200

    fixing the engine

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """State Module for HBNB project."""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """Represent State Model."""
 
     from models import storage_t
