a811ef5f788a038d24ad6a534d99c1c2098cf981
Aminekham <lkham0508@gmail.com>
2023-04-15 00:41:02 +0200

    fixing pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-First webapp using flask 
+First webapp using flask
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 strict_slashes = False
+
+
 @app.route("/")
 def hello_world():
     return("Hello HBNB!")
 
-if __name__ ==  '__main__':
+if __name__=='__main__':
     app.run(host='0.0.0.0', port='5000')
